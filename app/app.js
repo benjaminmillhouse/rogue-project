d3913dbc3ed65451f1d78b5b1b2105e8cc9938fa
Benjamin Millhouse <benmillhouse@CEN934.local>
2016-09-04 16:12:09 -0400

    added getMap, updateStatusBar and Character class

--- app/app.js
+++ app/app.js
@@ -1,17 +1,64 @@
 $(document).ready(function () {
-    function Character() {
-        this.name = name;
-    }
     var character = new Character();
     $('input[name="name-input"]').keypress(function (event) {
+        // If enter is pressed, save character name and load dungeon
         if (event.keyCode === 13) {
             var name = event.target.value;
-            alert('Welcome, ' + name + '!');
             character.name = name;
+            getMap('./dungeon.html');
         }
     });
 
-    $.get('./index.html', function () {
-        console.log(arguments);
-    });
+    /**
+     * This function loads the HTML file for the passed-in map, and udpates
+     * the view to show it.
+     *
+     * @param {string} url - The url of the map that you want to display
+     */
+    function getMap(url) {
+        $.get(url, function (data) {
+            $('#game-window').html(data);
+            $('#message-box').text('Welcome, ' + character.name + '!');
+            updateStatusBar();
+        });
+    }
+
+    /**
+     * Call this to update the values of the status bar
+     */    
+    function updateStatusBar() {
+        $('#sb-level').text(character.level);
+        $('#sb-hp').text(character.hp + '(' + character.hpMax + ')');
+        $('#sb-str').text(character.str + '(' + (character.str + character.strMod) + ')');
+        $('#sb-gold').text(character.gold);
+        $('#sb-armor').text(character.armor);
+        $('#sb-exp').text(character.exp + '/' + character.tnl);
+    }
 });
+
+/**
+ * @class
+ * This is the base Character class for the hero character
+ */
+function Character() {
+    this.armor = 5;
+    this.exp = 0;
+    this.gold = 0;
+    this.hp = 12;
+    this.hpMax = 12;
+    this.level = 1;
+    this.name = '';
+    this.str = 16;
+    this.strMod = 0;
+    this.tnl = 10;
+}
+/**
+ * This processes a level increase for the character
+ */
+Character.prototype.levelUp = function () {
+    this.level += 1;
+    this.hpMax += 10;
+    this.hp = this.hpMax;
+    this.str += 1;
+    this.exp = 0;
+};
