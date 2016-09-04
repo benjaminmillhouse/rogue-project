f49d0e3ac466481a6d4ce81dffbe08c744c07509
Benjamin Millhouse <benmillhouse@CEN934.local>
2016-09-04 15:15:57 -0400

    added 'enter' event for name field

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
+    function Character() {
+        this.name = name;
+    }
+    var character = new Character();
+    $('input[name="name-input"]').keypress(function (event) {
+        if (event.keyCode === 13) {
+            var name = event.target.value;
+            alert('Welcome, ' + name + '!');
+            character.name = name;
+        }
+    });
 
-
-    $('body').text('Welcome to Rogue!!!!!');
-
+    $.get('./index.html', function () {
+        console.log(arguments);
+    });
 });
